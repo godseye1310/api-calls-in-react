50af692041ba50af63bd0e352048cc11db0d6226
Avinash <avinashkmg1310@gmail.com>
2024-09-02 14:51:21 +0530

    fetch in async/await

--- src/App.js
+++ src/App.js
@@ -4,39 +4,42 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-	// const dummyMovies = [
-	//   {
-	//     id: 1,
-	//     title: 'Some Dummy Movie',
-	//     openingText: 'This is the opening text of the movie',
-	//     releaseDate: '2021-05-18',
-	//   },
-	//   {
-	//     id: 2,
-	//     title: 'Some Dummy Movie 2',
-	//     openingText: 'This is the second opening text of the movie',
-	//     releaseDate: '2021-05-19',
-	//   },
-	// ]; (JavaScript Object Notation)
-
 	const [movies, setMovies] = useState([]);
 
-	function fetchMoviesHandler() {
-		fetch('https://swapi.dev/api/films/')
-			.then((response) => {
-				return response.json();
-			})
-			.then((data) => {
-				const transformedMovies = data.results.map((movieData) => {
-					return {
-						id: movieData.episode_id,
-						title: movieData.title,
-						openingText: movieData.opening_crawl,
-						releaseDate: movieData.release_date,
-					};
-				});
-				setMovies(transformedMovies);
+	// function fetchMoviesHandler() {
+	// 	fetch('https://swapi.dev/api/films/')
+	// 		.then((response) => {
+	// 			return response.json();
+	// 		})
+	// 		.then((data) => {
+	// 			const transformedMovies = data.results.map((movieData) => {
+	// 				return {
+	// 					id: movieData.episode_id,
+	// 					title: movieData.title,
+	// 					openingText: movieData.opening_crawl,
+	// 					releaseDate: movieData.release_date,
+	// 				};
+	// 			});
+	// 			setMovies(transformedMovies);
+	// 		});
+	// }
+
+	async function fetchMoviesHandler() {
+		try {
+			const response = await fetch('https://swapi.dev/api/films/');
+			const data = await response.json();
+			const transformedMovies = await data.results.map((movieData) => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				};
 			});
+			setMovies(transformedMovies);
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
 	return (
